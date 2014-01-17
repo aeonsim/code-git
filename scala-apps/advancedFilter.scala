eabe75d0356d74caf89b8995fd0e30ced55efdac
Chad Harland <eonsim@gmail.com>
2014-01-18 00:18:51 +0100

    Generalisation improvements to RA script & first alpha version of advancedFilter.

--- scala-apps/advancedFilter.scala
+++ scala-apps/advancedFilter.scala
@@ -7,6 +7,8 @@
 * Check to see if each VCF line is the correct length & discard if not.
 */
 
+object advFilter{
+
 import java.io._
 import net.sf.samtools.util._
 import scala.collection.mutable.HashMap
@@ -30,22 +32,35 @@ return Nil
 def findChildren (pop: HashMap[String, Array[String]], vcf: HashMap[String,Int] , parent: String) : List[String]={
 var result: List[String] = Nil
 for (item <- pop){
-if (vcf.contains(item) && ( (item._2(2) == parent)||(item._2(3) == parent) )){
-result = parent :: result
+if (vcf.contains(item._1) && ( (item._2(2) == parent)||(item._2(3) == parent) )){
+result = item._1 :: result
 }//eif
 
 }//efor
 result
 }//edef
 
+def isHet(genotype:String): Boolean ={
+if (genotype.size == 3) {
+if ( genotype(0) != genotype(2)) true
+else false
+} else false
+}
+
 
 def main (args: Array[String]): Unit = {
 
-if (args.size <= 2) println("advFilter input.vcf.gz input.ped input_probands.txt minDP")
+if (args.size <= 2) {
+println("advFilter input.vcf.gz input.ped input_probands.txt minDP")
+System.exit(1)
+}
+println(s"Log file: ${args(2)}.log\n")
+
 
 val in_vcf = new BufferedReader(new InputStreamReader(new BlockCompressedInputStream(new FileInputStream(args(0)))))
 val in_ped = new BufferedReader(new FileReader(args(1)))
 val in_pro = new BufferedReader(new FileReader(args(2)))
+val log = new BufferedWriter(new FileWriter(args(2)+".log"))
 val minDP = args(3).toInt
 
 var pedFile = new HashMap[String, Array[String]]
@@ -58,11 +73,15 @@ var ancestors : List[String] = Nil
 var parents : List[String] = Nil
 var children : List[String] = Nil
 var descendents : List[String] = Nil
+var AD = -1
+var GT = -1
+var DP = -1
 
 /*
 * Iterate through VCF file to find Column headers and store positions in Map for later recall
 */
 
+//println("Start VCF ID extraction")
 
 var vcfrec = in_vcf.readLine().split("\t")
 while (vcfrec(0).apply(1) == '#'){
@@ -75,6 +94,8 @@ vcfanimals += vcfrec(i) -> i
 }
 }
 
+val animalIDS = vcfanimals.keys.toList.slice(9,vcfanimals.size)
+println(animalIDS + " Got Slice")
 /*
 * Build Map file of Pedigree
 */
@@ -95,27 +116,102 @@ while (in_pro.ready){
 val curPro = in_pro.readLine()
 
 if (pedFile.contains(curPro)){
-ancestors = itPed(pedFile,vcfanimals,curPro)
+ancestors = itPed(pedFile,vcfanimals,curPro).tail
 parents = pedFile(curPro)(2) :: pedFile(curPro)(3) :: Nil
 children = findChildren(pedFile,vcfanimals,curPro)
 
 for (kid <- children){
-descendents = findChildren(kid) 
+descendents = findChildren(pedFile,vcfanimals,kid) 
 }//efor
 
 trios += curPro -> (ancestors, parents, children, descendents)
 
 }//eif
 
-
 }//ewhile
 
 in_pro.close
 
-//Closed
+//Closed proband file
+
+/*
+*	Iterate through VCF file line by line, at each line load each Trio and count existence of variants in different categories
+*	if de novo, flag and output snp detail and variant info, (count in pop, children ancestors etc)
+*/
+
+while (in_vcf.ready){
+var denovo = false
+val line = in_vcf.readLine().split("\t")
+
+val format = line(8).split(":")
+AD = format.indexOf("AD")
+GT = format.indexOf("GT")
+DP = format.indexOf("DP")
+
+//println(line.size + " " + vcfanimals.size)
+if (line.size == vcfanimals.size){
+
+for (fam <- trios){
+//println(fam + " in fam loop")
+val ped = fam._2
+var ances, par, kids, desc, popFreq = 0
+
+/*
+* Loop through each family group and record Hets
+*/
 
 
+for (indv <- ped._1){
+if (isHet(line(vcfanimals(indv)).split(":").apply(GT))){
+ances += 1
+}
+}//Efor ansc
 
+for (indv <- ped._2){
+if (isHet(line(vcfanimals(indv)).split(":").apply(GT))){
+par += 1
+}
+}
 
+for (indv <- ped._3){
+if (isHet(line(vcfanimals(indv)).split(":").apply(GT))){
+kids += 1
+}
+}
+
+for (indv <- ped._4){
+if (isHet(line(vcfanimals(indv)).split(":").apply(GT))){
+desc += 1
+}
+}
+
+for (indv <- animalIDS){
+if (isHet(line(vcfanimals(indv)).split(":").apply(GT))){
+popFreq += 1
+}
+}
+
+/*
+* Check Pedigree segregation pattern
+*/
+log.write(s"${line(0)}\t${line(1)}\t${ances}\t${par}\t${kids}\t${desc}\n")
+log.write(line(vcfanimals(fam._1)).split(":").apply(GT) + "\t" + isHet(line(vcfanimals(fam._1)).split(":").apply(GT)) + "\n")
+
+if (isHet(line(vcfanimals(fam._1)).split(":").apply(GT)) && ances == 0 && par == 0 && kids >= 1 && ((kids + desc + 1) == popFreq)){
+denovo = true
+println(s"Denovo\t${line(0)}\t${line(1)}\tTrio:\t${fam._1}\t${line(vcfanimals(fam._1)).split(":").apply(GT)}\t${ances}\t${par}\t${kids}\t${desc}\n")
+}
+
+}//Efor fam <- trios
+
+} else {
+println(s"Error ${line(0)} ${line(1)} ${line.size}")
+} //else
+
+}// Ewhile
+
+log.close
+in_vcf.close
 }//eMain
 
+}//eObject
--- scala-apps/convertRA_to_vcf.scala
+++ scala-apps/convertRA_to_vcf.scala
@@ -3,8 +3,8 @@ import java.io._
 import scala.collection.mutable.HashMap
 
 val input = new BufferedReader(new InputStreamReader(new BlockCompressedInputStream(new FileInputStream("/Users/chhar0/Dropbox/PhD-Work/770k/770K-genotypes.vcf.gz"))))
-val genos = new BufferedReader(new FileReader("HD_chad_RA__0_0.txt"))
-val out = new BufferedWriter(new OutputStreamWriter(new BlockCompressedOutputStream("HD_out.vcf.gz")))
+val genos = new BufferedReader(new FileReader("AS_RA__0_0.txt"))
+val out = new BufferedWriter(new OutputStreamWriter(new BlockCompressedOutputStream("HD_out_anne-so.vcf.gz")))
 
 val raGenos = new HashMap[String, Array[String]]
 
@@ -14,18 +14,6 @@ var lineGenos = genos.readLine.split("\t")
 raGenos += ("Chr" + lineGenos(1) + ":" + lineGenos(2)) -> lineGenos
 
 }
-
-
-var vcfGeno = input.readLine
-
-while (vcfGeno(1) == '#'){
-out.write(vcfGeno + "\n")
-vcfGeno = input.readLine
-}
-
-var clArray = vcfGeno.split("\t")
-
-out.write(s"${clArray(0)}\t${clArray(1)}\t${clArray(2)}\t${clArray(3)}\t${clArray(4)}\t${clArray(5)}\t${clArray(6)}\t${clArray(7)}\t${clArray(8)}\t21930910\t22328328\t23934418\n")
 
 def raConv(in: String): String = {
 in match {
@@ -38,6 +26,22 @@ case _  => "./."
 }
 
 
+var vcfGeno = input.readLine
+
+while (vcfGeno(1) == '#'){
+out.write(vcfGeno + "\n")
+vcfGeno = input.readLine
+}
+
+var clArray = vcfGeno.split("\t")
+
+out.write(s"${clArray(0)}\t${clArray(1)}\t${clArray(2)}\t${clArray(3)}\t${clArray(4)}\t${clArray(5)}\t${clArray(6)}\t${clArray(7)}\t${clArray(8)}")
+val ids = raGenos("ChrChr:Position").apply(3).split(" ")
+for (i <- ids){
+out.write( "\t" + i)
+}
+out.write("\n")
+
 while (input.ready){
 clArray = input.readLine.split("\t")
 var key = clArray(0) + ":" + clArray(1)
@@ -45,8 +49,12 @@ if (raGenos.contains(key)){
 out.write(s"${clArray(0)}\t${clArray(1)}\t" + raGenos(key).apply(0) + s"\t${clArray(3)}\t${clArray(4)}\t${clArray(5)}\t${clArray(6)}\t${clArray(7)}\tGT")
 println(raGenos(key).apply(0))
 var ras = raGenos(key).apply(3)
-if (ras.size == 9){
-out.write(s"\t${raConv(ras.split(' ').apply(0))}\t${raConv(ras.split(' ').apply(2))}\t${raConv(ras.split(' ').apply(1))}\n")
+if (ras.size == 42){
+val tmp = ras.split(' ')
+
+for (i <- tmp){out.write( "\t" + raConv(i))}
+out.write("\n")
+//out.write(s"\t${raConv(.apply(0))}\t${raConv(ras.split(' ').apply(2))}\t${raConv(ras.split(' ').apply(1))}\n")
 } else {
 println("ERROR\t\t\t" + key + " " + ras)
 }
