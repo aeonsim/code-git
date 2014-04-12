9eadbca519baeddcb2ddf9d006dff07225d798c7
Chad Harland <eonsim@gmail.com>
2014-04-12 19:58:18 +0200

    NA > NV

--- scala-apps/hetInfo.scala
+++ scala-apps/hetInfo.scala
@@ -41,7 +41,7 @@ val details = line(testLoc).split(":")
 val format = line(8).split(":")
 val GT = details(format.indexOf("GT"))
 val REF = details(format.indexOf("NR")).toInt
-val ALT = details(format.indexOf("NA")).toInt
+val ALT = details(format.indexOf("NV")).toInt
 //val DTH = details("DP")
 
 val trueGT = truthAnimals(line(0) + ":" + line(1))(0)
