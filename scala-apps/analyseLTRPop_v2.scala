3fae6c6aa4185cd01a3653c9bbdff7681ba26edb
Chad Harland <eonsim@gmail.com>
2015-11-30 16:55:55 +0100

    Further formating improvements

--- scala-apps/analyseLTRPop_v2.scala
+++ scala-apps/analyseLTRPop_v2.scala
@@ -290,7 +290,7 @@ for (chr <- chromOrder){
 		if (tmp._4.contains(2)){  // require at least one individual to have both breakpoints
 			val sortedBreaks = if (tmp._9.size >= 2) tmp._9.toSeq.sortBy(- _._2).toArray else Array(tmp._9.head, (-1,0))
 			analysis.write(s"${chr}:${pos}-${pos + 1500}\t${tmp._1/populationSize.toFloat}\t${tmp._3}\t${tmp._5}\t${tmp._1}")
-			analysis.write(s"\t" + pedEventType(tmp._2) + s"\t${tmp._7(0)}\t${tmp._7(1)}\t${tmp._7(2)}\t${tmp._7(3)}\t${scala.math.abs(sortedBreaks(0)._1 - sortedBreaks(1)._1)}\t${tmp._6.filterNot(s => s == 0)},\t")
+			analysis.write(s"\t" + pedEventType(tmp._2) + s"\t${tmp._7(0)}\t${tmp._7(1)}\t${tmp._7(2)}\t${tmp._7(3)}\t${scala.math.abs(sortedBreaks(0)._1 - sortedBreaks(1)._1)}\t${tmp._6.filterNot(s => s == 0)}\t")
 			analysis.write(if (sortedBreaks(0)._1 <  sortedBreaks(1)._1) s"${sortedBreaks(0)._1}-${sortedBreaks(1)._1}" else s"${sortedBreaks(1)._1}-${sortedBreaks(0)._1}")
 			analysis.write(s"\t${tmp._10}\t")
 			tmp._8.foreach(s => analysis.write(s"${s._1},${s._2._1},${s._2._2}:"))
